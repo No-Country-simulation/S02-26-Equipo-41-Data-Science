92a6c75aeea29827d813b2c050f717047d2f1650
Enzo Zambon <eazambon01@gmail.com>
2026-02-16 17:38:22 -0300

    chore: cleanup broken tests and fix backend expectations

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Jenkins, por favor frena este deploy');
+      expect(appController.getHello()).toBe('Hello World!');
     });
   });
 });
