9b99ba5eb53879a1dd43a372d91803b607b6bbc7
Franco Kumichel <francokumichel1996@gmail.com>
2026-03-02 21:28:38 -0300

    feat(model): modifico modelo de cliente

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NoCountry API')
+    .setDescription('Documentación de la API del proyecto')
+    .setVersion('1.0')
+    .addTag('nocountry')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
